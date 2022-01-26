712d2e01fdae70b7eb0ce59ef90742dce26d4172
Anji <nhu.nhu.face@gmail.com>
2022-01-26 16:44:02 +0900

    add type to Screen(route), route params example in Login screen

--- app/routes/router.tsx
+++ app/routes/router.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../screens/home';
-import Login from '../screens/login';
+import React from "react";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/home";
+import Login from "../screens/login";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParams = {
+  Home: any;
+  Login: {
+    name: string;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParams>();
 const Router = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{
-                headerShown: false,
-            }}>
-                <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
-export default Router
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default Router;
--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -1,73 +1,89 @@
-import React, { useState, useCallback } from 'react'
-import { FlatList, View, Text, TextInput, TouchableOpacity, StyleSheet, Button, Alert } from 'react-native'
-import Header from '../components/Header'
-import CButton from '../components/CButton'
-import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
+import React, { useState, useCallback } from "react";
+import {
+  FlatList,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Button,
+  Alert,
+} from "react-native";
+import Header from "../components/Header";
+import CButton from "../components/CButton";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParams } from "../routes/router";
 
-const user_baseURL = "http://192.168.43.234:5000/api/v1/user";
-interface LoginScreenProps {
-  navigation: any;
-}
+// const user_baseURL = "http://192.168.43.234:5000/api/v1/user";
+const user_baseURL = "http://192.168.0.101:5000/api/v1/user";
+
+type Props = NativeStackScreenProps<RootStackParams, "Login">;
 
 //Login screen start from here!!
-const Login = (prop: LoginScreenProps) => {
+const Login: React.FC<Props> = (prop) => {
   const [username, setUsername] = useState("");
 
   //login button function // variable scope
   const login = async () => {
     if (!username) {
-      console.log('missing username input')
+      console.log("missing username input");
     } else {
       axios({
         method: "post",
         url: `${user_baseURL}/login/${username}`,
-      }).then((res) => {
-        return res.data.account_status;
-      }).then((status) => {
-        if (status) {
-          //login success
-          try {
-            //save username to secureStore
-            SecureStore.setItemAsync("username", username).then(() => {
-              prop.navigation.navigate('Home');
-            })
-          } catch (e) {
-            console.log(e)
-          }
-        }
-        else {
-          //login failed
-          console.log("Please register this username to login.")
-        }
       })
+        .then((res) => {
+          return res.data.account_status;
+        })
+        .then((status) => {
+          if (status) {
+            //login success
+            try {
+              //save username to secureStore
+              SecureStore.setItemAsync("username", username).then(() => {
+                prop.navigation.navigate("Home");
+              });
+            } catch (e) {
+              console.log(e);
+            }
+          } else {
+            //login failed
+            console.log("Please register this username to login.");
+          }
+        });
     }
-  }
+  };
 
   const register = async () => {
     if (!username) {
-      console.log('missing username input')
+      console.log("missing username input");
     } else {
       axios({
         method: "post",
         url: `${user_baseURL}/register/${username}`,
-      }).then((res) => {
-        return res.data.register_status;
-      }).then((status) => {
-        if (status) {
-          //Register success
-          console.log("Register success, please login with the username.")
-        } else {
-          //account already existed
-          console.log("Username has been registered, please login or change a username.")
-        }
       })
+        .then((res) => {
+          return res.data.register_status;
+        })
+        .then((status) => {
+          if (status) {
+            //Register success
+            console.log("Register success, please login with the username.");
+          } else {
+            //account already existed
+            console.log(
+              "Username has been registered, please login or change a username."
+            );
+          }
+        });
     }
-  }
+  };
 
   const changeHandlere = (val: string) => {
-    setUsername(val)
-  }
+    setUsername(val);
+  };
 
   return (
     <View>
@@ -76,7 +92,7 @@ const Login = (prop: LoginScreenProps) => {
         <Text>Enter name : </Text>
         <TextInput
           style={styles.input}
-          placeholder='User Name'
+          placeholder="User Name"
           onChangeText={changeHandlere}
         />
         <Button
@@ -85,60 +101,57 @@ const Login = (prop: LoginScreenProps) => {
           color="#841584"
         />
         <View style={styles.little_space}></View>
-        <CButton onPress={() => login()} >
-          Login
-        </CButton>
+        <CButton onPress={() => login()}>Login</CButton>
         <View style={styles.little_space}></View>
-        <Button
-          onPress={() => register()}
-          title="Register"
-          color="#841584"
-        />
-        <CButton onPress={() => {
-          Alert.alert(
-            "Error",
-            "Username must be more than 2 character",
-            [{ text: "Cancel", style: "cancel" }],
-            {
-              cancelable: true,
-            }
-          );
-        }} >test</CButton>
+        <Button onPress={() => register()} title="Register" color="#841584" />
+        <CButton
+          onPress={() => {
+            Alert.alert(
+              "Error",
+              "Username must be more than 2 character",
+              [{ text: "Cancel", style: "cancel" }],
+              {
+                cancelable: true,
+              }
+            );
+          }}
+        >
+          test
+        </CButton>
       </View>
-    </View >
-  )
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   content: {
     paddingTop: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     borderBottomWidth: 1,
-    borderColor: 'lightgrey',
+    borderColor: "lightgrey",
     padding: 8,
     margin: 10,
-    width: 200
+    width: 200,
   },
   data: {
     borderWidth: 1,
-    borderColor: '#777',
+    borderColor: "#777",
     height: 150,
   },
   login_btn: {
     width: 200,
     borderWidth: 2,
-    borderColor: 'skyblue',
+    borderColor: "skyblue",
     padding: 5,
     marginTop: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   little_space: {
     height: 5,
-  }
-
+  },
 });
 
-export default Login
+export default Login;
